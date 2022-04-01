9ac4ea1dbc82b907c6475e60ab2fc8d894d229d7
xiancli <xiancli@ebay.com>
2022-04-01 19:27:17 +0800

    update api response

--- service/stock_service.go
+++ service/stock_service.go
@@ -1,11 +1,18 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"weixin-golang/pkg"
 )
+
+type JsonResult struct {
+	Code     int         `json:"code"`
+	ErrorMsg string      `json:"errorMsg,omitempty"`
+	Data     interface{} `json:"data"`
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := getIndex()
@@ -18,13 +25,20 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func StockHandler(w http.ResponseWriter, r *http.Request) {
+	res := &JsonResult{}
 	result, err := pkg.FetchLastPrice("sz399001", "sz002594", "sz002230")
+	var b []byte
+	if err == nil {
+		res.Code = 200
+		res.Data = fmt.Sprintf("%v", result)
+		b, err = json.Marshal(res)
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 	} else {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(fmt.Sprintf("%v", result)))
+		w.Write(b)
 	}
 }
 
